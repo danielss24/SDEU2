d8df84acffd0865a8500c43158438157014ace74
danielss24 <daniels_s_24@hotmail.com>
2020-03-29 13:57:40 +0200

    Init

---
+++ app/src/main/java/com/example/domotica_sdeu_2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.domotica_sdeu_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
